7f3fc818985d279ca8224be3fe74d5fc31ea65da
Victor <victorhugo-brito@hotmail.com>
2025-01-30 08:48:42 -0300

    add project

---
+++ src/main/java/com/victorhgbb/siscoleta/siscoleta/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.victorhgbb.siscoleta.siscoleta;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SiscoletaApplication.class);
+	}
+
+}
---
+++ src/main/java/com/victorhgbb/siscoleta/siscoleta/SiscoletaApplication.java
@@ -0,0 +1,13 @@
+package com.victorhgbb.siscoleta.siscoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscoletaApplication.class, args);
+	}
+
+}
